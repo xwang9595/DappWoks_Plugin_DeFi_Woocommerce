e02f7f132fc8240738a74d3a62665c5d602be79b
xwang9595 <xwang6519@gmail.com>
2023-04-20 15:16:14 -0700

    commit dapp

---
+++ woo_dappWorks_defi_php/app/controller/Payment.php
@@ -0,0 +1,35 @@
+<?php
+namespace app\controller;
+
+use app\model\Order;
+use think\facade\Cache;
+
+class Payment extends Base
+{
+
+    public function home()
+    {
+       echo 'Hello';die;
+    }
+
+    public function index()
+    {
+        $d = input();
+        $order = Order::where('order_sn',$d['order_key'])->findOrEmpty()->toArray();
+        $data = ['code'=>1,'order'=>$order];
+        if(in_array($order['status'],['success','closed'])){
+            return view('pay', $data);
+        }
+        else{
+            return view('', $data);
+        }
+    }
+
+    public function pay()
+    {
+        $d = input();
+        $order = Order::where('order_sn',$d['order_key'])->findOrEmpty()->toArray();
+        $data = ['code'=>1,'order'=>$order];
+        return view('', $data);
+    }
+}
